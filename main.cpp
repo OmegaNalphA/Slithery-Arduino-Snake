28d849cbe138c123b2c0fbd922dccc9d4fec4a16
Anshul <agg_anshul@yahoo.com>
2016-10-27 13:49:36 -0700

    initial commit of private branch

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 //  main.cpp
 //  Snake
 //
-//  Created by Martin Kong on 10/27/16.
+//  Created by Martin Kong and Anshul Aggarwal on 10/27/16.
 //  Copyright © 2016 Martin Kong. All rights reserved.
 //
 
@@ -13,15 +13,25 @@
 
 using namespace std;
 
+enum dir {UP = 0, RIGHT = 1, DOWN = 2, LEFT = 3};
+enum color { OFF = 0, SNAKE = 1, APPLE = 2, WALL = 3};
+
 class Map{
 public:
     Map(){
         for(int i=0; i<8; i++){
             for(int j=0; j<8; j++){
-                arr[i][j] = 0;
+                arr[i][j] = OFF;
             }
         }
+		snake = new Snake(this, 0, 0);
+		apple = new Apple(this, 7, 7);
     }
+
+	int getSize() {
+		return this->size;
+	}
+
     int change(int x, int y, int change){
         if(x<8 && x>=0 && y<8 && y>=0){
             arr[x][y] = change;
@@ -39,22 +49,63 @@ public:
         arr[x_apple][y_apple] = 2;
         cout << x_apple << " " << y_apple << endl;
     }
+
+	void printMap() {
+		for (int i = 0; i < size; i++) {
+			for (int j = 0; j < size; j++) {
+				cout << arr[i][j];
+			}
+			cout << endl;
+		}
+	}
 private:
+	int size = 8;
     int arr[8][8];
+	Snake* snake;
+	Apple* apple;
     // 0 = empty, 1 = wall, 2 = apple
 };
 
-class Snake: public Map{
+class Snake{
 public:
+
+	Snake() {
+
+	}
+	
+	Snake(Map *map, int x, int y) {
+		length = 1;
+		direction = RIGHT;
+		m = map;
+		xPos = x;
+		yPos = y;
+		m->change(xPos, yPos, SNAKE);
+	}
+
     void createHead(){
         
     }
 private:
     int length;
     int direction;
-    int x;
-    int y;
+    int xPos;
+    int yPos;
     Map* m;
+};
+
+class Apple {
+public:
+	Apple() {
+	}
+	Apple(Map* map, int x, int y) {
+		m = map;
+		xPos = x;
+		yPos = y;
+		m->change(xPos, yPos, APPLE);
+	}
+private:
+	int xPos, yPos;
+	Map* m;
 };
 
 void run(){
@@ -62,13 +113,14 @@ void run(){
     if(m->change(0, 0, 1)==0){
         cout << "works" << endl;
     }
-    srand(time(NULL));
+    //srand(time(NULL));
     m->createApple();
 }
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    std::cout << "Hello, World!\n";
-    run();
+	Map* m = new Map();
+	m->printMap();
+    //run();
     return 0;
 }
